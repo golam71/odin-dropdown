ad7064b42586200bcf879c0137a0b023983dceac
golam71 <74001687+golam71@users.noreply.github.com>
2025-05-24 22:40:11 +0600

    :ambulance: Delete index.html

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-document.querySelectorAll(".dropdown-toggle").forEach((item) => {
-  item.addEventListener("click", function (e) {
-    let parent = e.target.parentNode;
-    let dropdownMenu = parent.querySelector(".dropdown-menu");
-    dropdownMenu.style.display =
-      dropdownMenu.style.display === "block" ? "none" : "block";
+export function initDropdowns() {
+  document.querySelectorAll(".dropdown-toggle").forEach((item) => {
+    item.addEventListener("click", function (e) {
+      let parent = e.target.parentNode;
+      let dropdownMenu = parent.querySelector(".dropdown-menu");
+      dropdownMenu.style.display =
+        dropdownMenu.style.display === "block" ? "none" : "block";
+    });
   });
-});
 
-let styles = document.createElement("style");
-let css = `
-.dropdown-menu{
-  display:none ;
+  let styles = document.createElement("style");
+  styles.textContent = `
+    .dropdown-menu {
+      display: none;
+    }
+  `;
+  document.head.append(styles);
 }
-`;
 
-styles.append(css);
-document.head.append(styles);
